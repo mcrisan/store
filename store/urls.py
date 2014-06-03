d64718e14f9892de104c432f87a13c2391ec1e67
mcrisan <crisan_mariusvlad@yahoo.com>
2014-06-03 14:57:30 +0300

    created login and register functionalities

--- store/urls.py
+++ store/urls.py
@@ -5,10 +5,23 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'webstore.views.home', name='home'),
+    url(r'^$', 'webstore.views.home', name='store_home'),
+    url(r'^register', 'webstore.views.register', name='register'),
     #url(r'^$', include('webstore.urls')),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^main/', include('webstore.urls')),
+)
+
+urlpatterns += patterns(
+    'django.contrib.auth.views',
+    
+    url(r'^login/$', 'login',
+        {'template_name': 'login.html'},
+        name='store_login'),
+    
+    url(r'^logout/$', 'logout',
+        {'next_page': 'store_home'},
+        name='store_logout'),  
 )
